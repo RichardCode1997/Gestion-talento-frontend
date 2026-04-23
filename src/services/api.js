002489db36943736d8e52f03bff2c967dc8db811
ericksson <ericksson12021997@gmail.com>
2026-04-23 18:03:11 -0500

    config prod Railway

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import router from '@/router'
 
 // ── Instancia base ──
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true
 })
